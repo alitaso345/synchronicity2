c8b3c60dfa61f20ef766e15902f6555c9a337ec6
alitaso345 <alice.maru345@gmail.com>
2020-06-14 20:53:42 +0900

    add setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,12 +17,16 @@ const convertToMessage = (res: Comment): Message => ({
 })
 
 export function App() {
+  const [hashTag, updateHashTag] = useState('#某isNight')
+  const [channel, updateChannel] = useState('#bou_is_twitch')
+  const [submit, updateSubmit] = useState(false)
   const [messages, update] = useState<Message[]>([])
 
   useEffect(() => {
+    console.log(hashTag, channel)
     const setting = new Setting()
-    setting.setHashTag('#アニポケ')
-    setting.setChannelName('#mogra')
+    setting.setHashTag(hashTag)
+    setting.setChannelName(channel)
 
     const timelineClient = new TimelineClient('http://localhost:8080', {}, {})
     const stream = timelineClient.connect(setting, {})
@@ -35,62 +39,88 @@ export function App() {
         return [message, ..._messages]
       })
     })
-  }, [])
+
+    return () => {
+      console.log('cancel streaming')
+      stream.cancel()
+    }
+  }, [submit])
 
   return (
-    <div>
-      {messages.map((item, index) => (
-        <div className="message-item" key={index}>
-          <span className="platform-icon">
-            <img
-              src={
-                item.platform === PlatformType.TWITTER
-                  ? '../public/Twitter_Social_Icon_Circle_Color.png'
-                  : '../public/TwitchGlitchPurple.png'
-              }
-              width={18}
-              height={18}
-            />
-          </span>
+    <>
+      <div>
+        <div>Twitterハッシュタグ</div>
+        <input
+          type="text"
+          value={hashTag}
+          onChange={e => updateHashTag(e.target.value)}
+        />
 
-          <span
-            className="user"
-            style={{
-              color: `${
-                item.platform === PlatformType.TWITTER ? '#00ACEE' : '#6441a5'
-              }`,
-              fontFamily: `Meiryo, system-ui, -apple-system, BlinkMacSystemFont, sans-serif`,
-              fontSize: '18px'
-            }}
-          >
-            {item.user}
-          </span>
+        <div>Twitchチャンネル</div>
+        <input
+          type="text"
+          value={channel}
+          onChange={e => updateChannel(e.target.value)}
+        />
 
-          <span className="colon">:</span>
-
-          {item.text.split(' ').map((t, i) => {
-            for (const emote of emoteData) {
-              if (emote.name === t) {
-                return (
-                  <img
-                    key={i}
-                    src={emote.url}
-                    width={emote.width}
-                    height={emote.height}
-                    alt={emote.name}
-                  />
-                )
-              }
-            }
-
-            return (
-              <span className="message" key={i}>
-                {t}
-              </span>
-            )
-          })}
+        <div>
+          <button onClick={() => updateSubmit(!submit)}>変更する</button>
         </div>
-      ))}
-    </div>
+      </div>
+      <div>
+        {messages.map((item, index) => (
+          <div className="message-item" key={index}>
+            <span className="platform-icon">
+              <img
+                src={
+                  item.platform === PlatformType.TWITTER
+                    ? '../public/Twitter_Social_Icon_Circle_Color.png'
+                    : '../public/TwitchGlitchPurple.png'
+                }
+                width={18}
+                height={18}
+              />
+            </span>
+
+            <span
+              className="user"
+              style={{
+                color: `${
+                  item.platform === PlatformType.TWITTER ? '#00ACEE' : '#6441a5'
+                }`,
+                fontFamily: `Meiryo, system-ui, -apple-system, BlinkMacSystemFont, sans-serif`,
+                fontSize: '18px'
+              }}
+            >
+              {item.user}
+            </span>
+
+            <span className="colon">:</span>
+
+            {item.text.split(' ').map((t, i) => {
+              for (const emote of emoteData) {
+                if (emote.name === t) {
+                  return (
+                    <img
+                      key={i}
+                      src={emote.url}
+                      width={emote.width}
+                      height={emote.height}
+                      alt={emote.name}
+                    />
+                  )
+                }
+              }
+
+              return (
+                <span className="message" key={i}>
+                  {t}
+                </span>
+              )
+            })}
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
